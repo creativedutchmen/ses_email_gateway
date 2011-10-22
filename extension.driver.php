662c27116e47c2ec5c7e58f3c1a6e606381b9e48
Huib Keemink <huib.keemink@creativedutchmen.com>
2011-10-22 23:40:44 +0200

    Initial commit, added extension driver.

---
+++ extension.driver.php
@@ -0,0 +1,17 @@
+<?php
+
+class extension_random_hash_field extends Extension{
+
+	public function about(){
+			return array(
+				'name' => 'Amazon SES - email gateway',
+				'version' => '0.1',
+				'release-date' => '2011-10-22',
+				'author' => array(
+					'name' => 'Huib Keemink',
+					'website' => 'http://www.creativedutchmen.com',
+					'email' => 'huib.keemink@creativedutchmen.com'
+				)
+			);
+		}
+}
